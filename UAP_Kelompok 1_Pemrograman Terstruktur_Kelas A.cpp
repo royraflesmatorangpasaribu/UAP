432cc25b9b03121645b8b2b70b47b88d6bf229e3
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-06-14 12:09:18 +0700

    membuat fungsi enqueue

--- UAP_Kelompok 1_Pemrograman Terstruktur_Kelas A.cpp
+++ UAP_Kelompok 1_Pemrograman Terstruktur_Kelas A.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
 
+using namespace std;
+
+queue<string>data;
+string noPolisi;
+
+void enqueue(){
+    do{
+    system("cls");
+    cout<<"Masukan Nomor Polisi  : ";
+    getline(cin, noPolisi);
+    if(noPolisi.empty()){
+        cout << "\nKendaraan tidak boleh Kosong!";
+        getch();
+    }
+    }while(noPolisi.empty());
+    data.push(noPolisi);
+}
+
 int main(){
 	
 }
